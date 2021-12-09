9dc6e7dd770bbffe99eae03725b86fd920aeb7e6
dearblue <dearblue@users.osdn.me>
2021-12-09 22:41:47 +0900

    Add `MRB_UTF8_STRING` test for CI

--- build_config/local.rb
+++ build_config/local.rb
@@ -13,3 +13,16 @@ MRuby::Build.new do |conf|
     # conf.gem core: 'mruby-bin-debugger'
   #end
 end
+
+MRuby::Build.new('utf8') do |conf|
+  toolchain :gcc
+  conf.compilers.each { |cc| cc.defines << 'MRB_UTF8_STRING' }
+  conf.gem core: 'mruby-bin-mruby'
+  conf.gem core: 'mruby-print'
+  conf.gem core: 'mruby-sprintf'
+  conf.gem File.expand_path('../../', __FILE__)
+  conf.gem core: 'mruby-bin-mrbc' if MRuby::Source::MRUBY_RELEASE_NO <= 30000
+
+  conf.enable_test
+  conf.enable_debug
+end
